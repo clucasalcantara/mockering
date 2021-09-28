0a6b6bc968665542b161a2c576821247deb00550
Caio Alcantara <alcantaracaiolucas@gmail.com>
2021-09-28 19:01:00 -0300

    feat: wip add datatypes module

---
+++ src/datatypes/datatypes.test.ts
@@ -0,0 +1,7 @@
+import datatypes from './datatypes'
+
+describe('Datatypes module', () => {
+  it('should return a random uuid', () => {
+    expect(datatypes().uuid()).toHaveLength(36)
+  })
+})
---
+++ src/datatypes/datatypes.ts
@@ -0,0 +1,35 @@
+export interface IDataTypes {
+  uuid(): string
+}
+
+const datatypes = (): IDataTypes => {
+  const generateUuid = (): string => {
+    const getGroupLength = (index: number) => {
+      if (index === 0) return 8
+      if (index === 4) return 12
+
+      return 4
+    }
+
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+
+    const uuid: string[] = []
+
+    for (let i = 0; i < 5; i++) {
+      const length = getGroupLength(i)
+      const word = []
+      for (var j = 0; j < length; j++) {
+        word.push(characters.charAt(Math.floor(Math.random() * characters.length)))
+      }
+      uuid.push(word.join(''))
+    }
+
+    return uuid.join('-')
+  }
+
+  return {
+    uuid: () => generateUuid(),
+  }
+}
+
+export default datatypes
